f1acf568173e2bf6ae25daa309def3b0fd89cc01
Aayush Chhikara <88643777+boltx393@users.noreply.github.com>
2023-08-18 19:52:33 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,54 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const stockForm = document.getElementById('stockForm');
-  const stockInput = document.getElementById('stockInput');
-  const stockChart = document.getElementById('stockChart').getContext('2d');
-  let chart = null;
-
-  stockForm.addEventListener('submit', function(e) {
-    e.preventDefault();
-    const stockName = stockInput.value;
-    fetchData(stockName);
-  });
-
-  function fetchData(stockName) {
-    const apiKey = document.currentScript.getAttribute('data-api-key');
-    const API_URL = `https://api.example.com/stocks/${stockName}/history?apiKey=${apiKey}`;
-
-    // Use your preferred method to fetch data, such as Fetch API or Axios
-    fetch(API_URL)
-      .then(response => response.json())
-      .then(data => createChart(data))
-      .catch(error => console.error('Error:', error));
-  }
-
-  function createChart(data) {
-    // Destroy previous chart if it exists
-    if (chart) {
-      chart.destroy();
-    }
-
-    chart = new Chart(stockChart, {
-      type: 'line',
-      data: {
-        labels: data.labels,
-        datasets: [
-          {
-            label: data.stockName,
-            data: data.values,
-            backgroundColor: 'rgba(75, 192, 192, 0.2)',
-            borderColor: 'rgba(75, 192, 192, 1)',
-            borderWidth: 1
-          }
-        ]
-      },
-      options: {
-        responsive: true,
-        scales: {
-          y: {
-            beginAtZero: true
-          }
-        }
-      }
+    const showMessageButton = document.getElementById('showMessageButton');
+    
+    showMessageButton.addEventListener('click', function() {
+        alert('Hello! This is an example message from the stock website.');
     });
-  }
 });
